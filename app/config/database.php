3baf0bb3d0e39bf3940ccccc37fbadff5978565f
Vincent Fleurette <vincent.fleurette@anaphore.eu>
2015-04-30 10:24:30 +0200

    Revert "add new logout element in the sidebar"
    This reverts commit 554c5c9c4609c2c895d812e30c77ee65e13fff97.

--- app/config/database.php
+++ app/config/database.php
@@ -1,18 +1,46 @@
 <?php
 
 $config['database'] = array(
-    'default'       => 'pgsql',
+    'default'       => 'sqlite',
 
     'connections'   => array(
-        'pgsql'     => array(
-            'driver'    => 'pgsql',
+
+        'mysql' => array(
+            'driver'    => 'mysql',
             'host'      => isset($_SERVER['DB1_HOST']) ? $_SERVER['DB1_HOST'] : 'localhost',
-            'database'  => isset($_SERVER['DB1_NAME']) ? $_SERVER['DB1_NAME'] : 'oaiphp',
-            'username'  => isset($_SERVER['DB1_USER']) ? $_SERVER['DB1_USER'] : 'oaiphp',
-            'password'  => isset($_SERVER['DB1_PASS']) ? $_SERVER['DB1_PASS'] : 'anaphore',
+            'database'  => isset($_SERVER['DB1_NAME']) ? $_SERVER['DB1_NAME'] : 'database',
+            'username'  => isset($_SERVER['DB1_USER']) ? $_SERVER['DB1_USER'] : 'root',
+            'password'  => isset($_SERVER['DB1_PASS']) ? $_SERVER['DB1_PASS'] : '',
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
-            'prefix'    => ''
-        )
+            'prefix'    => '',
+        ),
+
+        'sqlite' => array(
+            'driver'   => 'sqlite',
+            'database' => APP_PATH.'storage/db/database.sqlite',
+            'prefix'   => '',
+        ),
+
+        'pgsql' => array(
+            'driver'   => 'pgsql',
+            'host'     => 'localhost',
+            'database' => 'database',
+            'username' => 'root',
+            'password' => '',
+            'charset'  => 'utf8',
+            'prefix'   => '',
+            'schema'   => 'public',
+        ),
+
+        'sqlsrv' => array(
+            'driver'   => 'sqlsrv',
+            'host'     => '127.0.0.1',
+            'database' => 'database',
+            'username' => 'user',
+            'password' => '',
+            'prefix'   => '',
+        ),
+
     )
 );
