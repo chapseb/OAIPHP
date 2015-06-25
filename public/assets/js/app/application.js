4ab452fc81ea608ee99ce1a20b8f6861030ad481
Sebastien Chaptal <sebastien.chaptal@anaphore.eu>
2015-06-25 10:29:14 +0200

    Display manage files
    Add
    Delete
    List set and files
    Javascript stuff for checking manage files

---
+++ public/assets/js/app/application.js
@@ -0,0 +1,48 @@
+$(function() {
+    $("#checkAll").click(function() {
+        $(".check").prop('checked', $(this).prop('checked'));
+    });
+
+    $('#myModal').on('shown.bs.modal', function () {
+        var listFiles = [];
+        $('input:checked').each(function() {
+              listFiles.push($(this).val());
+        });
+        if( listFiles.indexOf("Tout cocher") != -1 ) {
+            $("#listFilesConfirm").html("Vous avez selectionné tous les fichiers");
+            $("#confirmButton").removeAttr("disabled");
+        }
+        else if (listFiles.length < 1 ){
+            $("#listFilesConfirm").html("Vous n'avez selectionné aucun fichier");
+            $("#confirmButton").attr("disabled", "disabled");
+        }
+        else {
+            var newHTML = [];
+            $.each(listFiles, function(index, value) {
+                    newHTML.push('<span>' + value + '</span>');
+            });
+            $("#listFilesConfirm").html("Vous avez sélectionné les fichiers suivants :<br />" + newHTML.join("<br />"));
+            $("#confirmButton").removeAttr("disabled");
+        }
+        $('#myInput').focus();
+    });
+
+    $('#myModalDelete').on('shown.bs.modal', function () {
+        var listFiles = [];
+        $('input:checked').each(function() {
+              listFiles.push($(this).val());
+        });
+        if( listFiles.indexOf("Tout cocher") != -1 ) {
+            $("#deleteFilesConfirm").html("Vous avez selectionné tous les élements");
+        }
+        else {
+            var newHTML = [];
+            $.each(listFiles, function(index, value) {
+                    newHTML.push('<span>' + value + '</span>');
+            });
+            $("#deleteFilesConfirm").html("Vous avez sélectionné les fichiers suivants :<br />" + newHTML.join("<br />"));
+        }
+        $('#myInput').focus();
+    });
+
+});
